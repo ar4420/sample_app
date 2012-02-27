36f4508cf860d7dc910c9674c0232ee8292f6cdd
Arian <arian.radmand@gmail.com>
2012-02-27 09:00:42 -0500

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,12 +16,6 @@ describe "User pages" do
 
     before { visit signup_path }
 
-    describe "with invalid information" do
-      it "should not create a user" do
-        expect { click_button "Sign up" }.not_to change(User, :count)
-      end
-    end
-
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
@@ -33,6 +27,20 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button "Sign up" }.to change(User, :count).by(1)
       end
+
+      it "should not create a user" do
+        expect { click_button "Sign up" }.not_to change(User, :count)
+      end
+
+      describe "after saving the user" do
+        before { click_button "Sign up" }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.flash.success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+
     end
   end
 end
